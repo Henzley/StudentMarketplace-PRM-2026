a524905638eee3b9e9abce8c4b5d3c9a8d73de6b
Safiya Elmi <240500598@mycput.ac.za>
2026-08-20 08:42:41 +0200

    Updated user table

--- data/studentmarketplace.sql
+++ data/studentmarketplace.sql
@@ -189,6 +189,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `lastName` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
+  `role` enum('Admin','User') NOT NULL DEFAULT 'User',
   PRIMARY KEY (`userID`),
   UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
